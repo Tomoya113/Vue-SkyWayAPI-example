da452ec495c2b302eafbc70a258ab086f8d4a904
Tanaka-Tomoya <tomoya2627@gmail.com>
2020-08-26 01:32:09 +0900

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,79 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <video @loadeddata="onLoadedData" :srcObject.prop="myStream" id="my-video" width='400px' autoplay muted playsinline></video>
+    <p id="my-id">
+      {{ peerId }}
+    </p>
+
+    <textarea id="their-id" v-model="theirId"></textarea>
+    <button id="make-call" @click="makeCall">発信</button>
+    <video @loadeddata="onLoadedData" :srcObject.prop="theirStream" id="their-video" width="400px" autoplay muted playsinline></video>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Peer from 'skyway-js'
+import { APIKEY } from '../key';
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      peerId: "",
+      theirId: "",
+      localStream: null,
+      peer: null,
+      mediaConnection: null,
+      myStream: null,
+      theirStream: null,
+    }
+  },
+  created() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+      .then(stream => {
+        console.log({stream})
+        this.myStream = stream
+        this.localStream = stream;
+      }).catch(error => {
+        console.error('mediaDevice.getUserMedia() error:', error);
+        return;
+      })
+    console.log(this.localStream)
+
+    this.peer = new Peer({
+      key: APIKEY,
+      debug: 3,
+    })
+
+    this.peer.on('open', () => {
+      console.log(this.peer.id)
+      this.peerId = this.peer.id;
+    })
+
+    this.peer.on('call', mediaConnection => {
+      mediaConnection.answer(this.localStream);
+    })
+  },
+  methods: {
+    makeCall() {
+      console.log(this.theirId)
+      this.mediaConnection = this.peer.call(this.theirId, this.localStream);
+      this.mediaConnection.on('stream', stream => {
+        this.theirStream = stream;
+        console.log(this.theirStream)
+        console.log(this.myStream);
+      })
+    },
+    onLoadedData(event) {
+      console.log(event)
+      event.target.play();
+    },
+    // setEventListener(mediaConnection) {
+      // mediaConnection.on('stream', stream => {
+      //   this.theirStream = stream;
+      //   console.log(this.theirStream)
+      //   console.log(this.myStream);
+      // })
+    // }
+  },
 }
 </script>
